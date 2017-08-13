9c46b249abaa5507faceb2cf9b366e441afe9a5f
Túlio de Oliveira <tulio.ao@gmail.com>
2017-08-13 17:51:24 -0300

    1.0.3

--- app.js
+++ app.js
@@ -3,16 +3,18 @@ var bodyParser = require('body-parser');
 var mongoose   = require('mongoose');
 var app        = express();
 
-app.use(bodyParser.urlencoded({extended: true}));
-app.set("view engine", "ejs");
-
-//DB setup
+// Database Setup
 mongoose.Promise = global.Promise;
 mongoose.connect("mongodb://mongo:27017", {
 	keepAlive: true,
 	reconnectTries: Number.MAX_VALUE,
 	useMongoClient: true
 });
+
+// App Config
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get('/', function(req, res){
   res.send("Hello World");
