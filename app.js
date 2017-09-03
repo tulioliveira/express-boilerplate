c0b0a09268d041b0b5d0baa91a433c6a23211c3d
Túlio de Oliveira <tulio.ao@gmail.com>
2017-09-03 14:05:04 -0300

    1.3.0
    Added Login and User data, as well as middleware for checking login

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
-var express          = require('express');
-var bodyParser       = require('body-parser');
-var expressSanitizer = require('express-sanitizer');
-var mongoose         = require('mongoose');
-var methodOverride   = require('method-override');
-var seedDB           = require("./seeds");
-var app              = express();
+var express               = require('express');
+var bodyParser            = require('body-parser');
+var expressSanitizer      = require('express-sanitizer');
+var mongoose              = require('mongoose');
+var methodOverride        = require('method-override');
+var passport              = require('passport');
+var LocalStrategy         = require('passport-local');
+var passportLocalMongoose = require('passport-local-mongoose');
+// var seedDB             = require("./seeds");
+var app                   = express();
 
 // Database Setup
 mongoose.Promise = global.Promise;
@@ -20,16 +23,39 @@ app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(expressSanitizer());
 app.use(methodOverride("_method"));
+app.use(require("express-session")({
+	secret: "Custom Secret Text for Your App",
+	resave: false,
+	saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
 
 // Database Models
 var User = require('./models/user');
 
-seedDB();
+// Passport Setup
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
-app.get('/', function(req, res){
+// Database Seeding
+// seedDB();
+
+/**
+ * Routes
+ */
+app.get('/', function (req, res) {
 	res.render("index");
 });
 
-app.listen(3000, function(){
+app.listen(3000, function () {
 	console.log('Example app listening on port 3000!');
 });
+
+function isLoggedIn(req, res, next) {
+	if (req.isAuthenticated()) {
+		return next();
+	}
+	res.redirect('/login');
+}
