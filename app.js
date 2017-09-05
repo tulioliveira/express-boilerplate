ef884026f896a1394b2e21fa32616f3f39ed686f
Túlio de Oliveira <tulio.ao@gmail.com>
2017-09-05 18:06:23 -0300

    1.4.0
    Changed from EJS to Handlebars Template Engine. Added Basic Auth Logic with Users and Cleaned up the Routers path

--- app.js
+++ app.js
@@ -1,14 +1,22 @@
-var express               = require('express');
-var bodyParser            = require('body-parser');
-var expressSanitizer      = require('express-sanitizer');
-var mongoose              = require('mongoose');
-var methodOverride        = require('method-override');
-var passport              = require('passport');
-var LocalStrategy         = require('passport-local');
-// var seedDB             = require("./seeds");
-var app                   = express();
+var express          = require('express');
+var exphbs           = require('express-handlebars');
+var bodyParser       = require('body-parser');
+var expressSanitizer = require('express-sanitizer');
+var mongoose         = require('mongoose');
+var methodOverride   = require('method-override');
+var passport         = require('passport');
+var LocalStrategy    = require('passport-local');
+// var seedDB        = require("./seeds");
+var app              = express();
 
-// Database Setup
+/**
+ * Routers
+ */
+var indexRoutes = require('./routes/index');
+
+/**
+ * Database Setup
+ */
 mongoose.Promise = global.Promise;
 mongoose.connect("mongodb://mongo:27017/project_db_name", {
 	keepAlive: true,
@@ -16,45 +24,52 @@ mongoose.connect("mongodb://mongo:27017/project_db_name", {
 	useMongoClient: true
 });
 
-// App Config
-app.set("view engine", "ejs");
+/**
+ * App Configuration
+ */
+app.engine('handlebars', exphbs({
+	defaultLayout: 'main',
+	helpers: require("./public/js/helpers.js").helpers
+}));
+app.set('view engine', 'handlebars');
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(expressSanitizer());
 app.use(methodOverride("_method"));
+
+/**
+ * Database Models
+ */
+var User = require('./models/user');
+
+/**
+ * Passport Configuration
+ */
 app.use(require("express-session")({
-	secret: "Custom Secret Text for Your App",
+	secret: "Custom Secret Seed Text for Your App",
 	resave: false,
 	saveUninitialized: false
 }));
 app.use(passport.initialize());
 app.use(passport.session());
-
-// Database Models
-var User = require('./models/user');
-
-// Passport Setup
 passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
+app.use(function(req, res, next) {
+	res.locals.currentUser = req.user;
+	next();
+});
 
-// Database Seeding
+/**
+ * Database Seeding
+ */
 // seedDB();
 
 /**
- * Routes
+ * Routes Setup
  */
-app.get('/', function (req, res) {
-	res.render("index");
-});
+app.use("/", indexRoutes);
 
 app.listen(3000, function () {
 	console.log('Example app listening on port 3000!');
 });
-
-function isLoggedIn(req, res, next) {
-	if (req.isAuthenticated()) {
-		return next();
-	}
-	res.redirect('/login');
-}
---
+++ routes/index.js
@@ -0,0 +1,69 @@
+var express  = require('express');
+var router   = express.Router();
+var passport = require('passport');
+var User     = require('../models/user');
+
+//==============
+// INDEX ROUTES
+//==============
+
+/**
+ * MAIN PAGE
+ */
+
+// Home Page
+router.get('/', function(req,res){
+	res.render('index');
+});
+
+/**
+ * AUTH ROUTES
+ */
+
+// Sign Up Form
+router.get('/register', function (req, res) {
+	res.render('register');
+});
+
+// Sign Up Logic Handling
+router.post('/register', function (req, res) { 
+	var newUser = new User({username: req.body.username});
+	User.register(newUser, req.body.password, function(err, user) {
+		if (err) {
+			console.log(err);
+			return res.render("register");
+		}
+		passport.authenticate("local")(req, res, function() {
+			res.redirect('/');
+		});
+	});
+});
+
+// Login Form
+router.get('/login', function (req, res) {
+	res.render('login');
+});
+
+// Login Logic Handling
+router.post('/login', passport.authenticate('local', {
+	successRedirect: '/',
+	failureRedirect: '/login'
+}), function (req, res) {});
+
+// Logout Logic Handling
+router.get('/logout', function(req, res) {
+	req.logout();
+	res.redirect('/');
+});
+
+/**
+ * Auth Function
+ */
+function isLoggedIn(req, res, next) {
+	if(req.isAuthenticated()){
+		return next();
+	}
+	res.redirect('/login');
+}
+
+module.exports = router;
